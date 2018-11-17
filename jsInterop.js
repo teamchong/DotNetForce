b4a6dfd505fe1e86a6ebcaad20ab2297ab20ff75
Steven Chong <steven.chong@ubm.com>
2018-11-17 11:57:25 +0800

    website

---
+++ jsInterop.js
@@ -0,0 +1,31 @@
+window.jsInterop = {
+    openAuthWindow: function (url) {
+        open(url, '_blank', 'width=600,height=800');
+    },
+    getStorage: function (key) {
+        return sessionStorage.getItem(key);
+    },
+    setStorage: function (key, value) {
+        sessionStorage.setItem(key, value);
+    },
+    waitForForSession: function (key, callback) {
+        requestAnimationFrame(function () {
+            var value = sessionStorage.getItem(key);
+            if (value) {
+                var result = callback.invokeMethodAsync('Callback', key, value);
+                if (value != result) {
+                    sessionStorage.setItem(key, result);
+                }
+            }
+            jsInterop.waitForForSession(key, callback);
+        });
+    },
+    addEventListener: function (type, callback) {
+        addEventListener(type, function (ev) {
+            var result = callback.invokeMethodAsync('Callback', type, JSON.stringify(ev));
+            if (result === 'false') {
+                return false;
+            }
+        });
+    }
+};
